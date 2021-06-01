a796a45dc6ff67013fbd5eaac19a072c9242d8c9
hisamparker <hisamparker@gmail.com>
2021-06-01 11:53:51 +0200

    used method override to create a PUT rout for editing spots

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const hbs = require('hbs');
 const path = require('path');
+const methodOverride = require('method-override');
 
 //local require
 const Spot = require('./models/spot.model');
@@ -38,6 +39,7 @@ app.set('views', path.join(__dirname, 'views'));
 //parse the data coming in
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res, next) => {
   res.render('home');
--- routes/spots.routes.js
+++ routes/spots.routes.js
@@ -1,18 +1,18 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = new Router();
 
-const Spot = require("../models/spot.model");
+const Spot = require('../models/spot.model');
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const spots = await Spot.find({});
-  res.render("spots/index", { spots });
+  res.render('spots/index', { spots });
 });
 
-router.get("/new", (req, res, next) => {
-  res.render("spots/new");
+router.get('/new', (req, res, next) => {
+  res.render('spots/new');
 });
 
-router.post("/new", async (req, res, next) => {
+router.post('/new', async (req, res, next) => {
     const {name, location, description} = req.body;
     const newSpot = new Spot({name, location, description});
     await newSpot.save();
@@ -20,10 +20,29 @@ router.post("/new", async (req, res, next) => {
     res.redirect(`${newSpot._id}`);
 });
 
-//remember to put other routes prefixed by / before this route or their will be a load error because what's after the route will be read by the client as an id
-router.get("/:id", async (req, res, next) => {
+router.get('/:id/edit', async (req, res, next) => {
   const spot = await Spot.findById(req.params.id);
-  res.render("spots/show", { spot });
+  res.render('spots/edit', { spot });
 });
+
+router.put('/:id/edit', async (req, res, next) => {
+    const { id } = req.params;
+    const { name, location, description } = req.body;
+    console.log(name, location, description)
+    const updatedSpot = await Spot.findByIdAndUpdate(id, {
+      name,
+      location,
+      description,
+    });
+    await updatedSpot.save();
+    res.redirect(`/spots/${updatedSpot._id}`);
+});
+
+//remember to put other routes prefixed by / before this route or their will be a load error because what's after the route will be read by the client as an id
+router.get('/:id', async (req, res, next) => {
+  const spot = await Spot.findById(req.params.id);
+  res.render('spots/show', { spot });
+});
+
 
 module.exports = router;
