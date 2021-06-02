6bd6a7b3ab71415060c4e22dd377bcbc1aa6db3a
hisamparker <hisamparker@gmail.com>
2021-06-02 08:30:23 +0200

    installed ESLint w/Prettier to be less shitty, yay

--- app.js
+++ app.js
@@ -1,75 +1,68 @@
 require('dotenv').config();
 
-//dependencies
+// dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const hbs = require('hbs');
 const path = require('path');
 const methodOverride = require('method-override');
 
-//local require
-const Spot = require('./models/spot.model');
+// local require
+// const Spot = require('./models/spot.model');
 const spotRoutes = require('./routes/spots.routes');
-const ErrorHandler = require('./utils/errors');
+// const ErrorHandler = require('./utils/errors');
 
-mongoose.connect(
-    `mongodb://localhost:27017/${process.env.DB_NAME}`,
-    {
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
-        useCreateIndex: true
-    }
-);
+        useCreateIndex: true,
+});
 
-//assign value of mongoose connection to a var to make it easier to reuse
+// assign value of mongoose connection to a var to make it easier to reuse
 const db = mongoose.connection;
-//bind console.error to the console object, then pass in a string to describe the location of the error (connection)
+// bind console.error to the console object, then pass in a string to describe the location of the error (connection)
 db.on('error', console.error.bind(console, 'connection error:'));
-//open the db!
+// open the db!
 db.once('open', () => {
-    console.log(`Database: ${process.env.DB_NAME} connected`);
+        console.log(`Database: ${process.env.DB_NAME} connected`);
 });
 
 const app = express();
 
 app.set('view engine', 'hbs');
-//provides path to views - we always want the file we're trying to access the view from to be able to reach views
+// provides path to views - we always want the file we're trying to access the view from to be able to reach views
 app.set('views', path.join(__dirname, 'views'));
 // Path to the location for handlebars partials here:
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-//parse the data coming in
+// parse the data coming in
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
-
 app.get('/', (req, res, next) => {
-    console.log('(-_-｡)');
-  res.render('home');
+        console.log('(-_-｡)');
+        res.render('home');
 });
 
 app.use('/spots', spotRoutes);
 
-
-
-app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'this is not good (-_-｡)' } = err;
-    res.status(statusCode).render('error', { err });
-});
+// app.use((err, req, res, next) => {
+//     const { status = 500, message = 'this is not good (-_-｡)' } = err;
+//     res.status(status).send(message);
+// });
 
 // app.all('*', (req, res, next) => {
 //   next(new ErrorHandler('Page not found (-_-｡)', 404));
 // });
-
 
 // app.use((err, req, res, next) => {
 //   handleError(err, res);
 // });
 
 app.listen(process.env.PORT, () => {
-  console.log(`listening on ${process.env.PORT}`);
+        console.log(`listening on ${process.env.PORT}`);
 });
 
 module.exports = app;
--- routes/spots.routes.js
+++ routes/spots.routes.js
@@ -3,6 +3,7 @@ const router = new Router();
 const { DateTime } = require('luxon');
 
 const Spot = require('../models/spot.model');
+const ErrorHandler = require("../utils/errors");
 
 router.get('/', async (req, res, next) => {
   const spots = await Spot.find({});
@@ -17,21 +18,17 @@ router.post('/new', async (req, res, next) => {
     const {name, location, image, description} = req.body;
     const newSpot = new Spot({name, location, image, description});
     await newSpot.save();
-    console.log(newSpot)
     res.redirect(`${newSpot._id}`);
 });
 
 router.get('/:id/edit', async (req, res, next) => {
   const spot = await Spot.findById(req.params.id);
-  console.log(spot)
   res.render('spots/edit', { spot });
 });
 
 router.put('/:id/edit', async (req, res, next) => {
     const { id } = req.params;
     const { name, location, image, description } = req.body;
-    console.log(name, location, image, description)
-    console.log(req.body)
     const updatedSpot = await Spot.findByIdAndUpdate(id, {
       name,
       location,
@@ -44,22 +41,32 @@ router.put('/:id/edit', async (req, res, next) => {
 router.delete('/:id/delete', async (req, res, next) => {
     const { id } = req.params;
     const deletedSpot = await Spot.findByIdAndDelete(id);
-    console.log(deletedSpot);
     res.redirect('/spots');
 });
 
 //remember to put other routes prefixed by / before this route or their will be a load error because what's after the route will be read by the client as an id
 router.get('/:id', async (req, res, next) => {
-    const { id } = req.params;
-    const spot = await Spot.findById(id);
-    const updatedAt = spot.updated_at;
-    const formattedUpdatedAt = DateTime.fromJSDate(updatedAt).toFormat('yyyy LLL dd');
-    // const updatedAt = spot.updated_at.toString();
-    // console.log(updatedAt, typeof updatedAt);
-    // const formattedUpdatedAt = updatedAt.split(" ");
-    // formattedUpdatedAt.slice(4);
-    console.log(formattedUpdatedAt);
-    res.render("spots/show", { spot, updatedAt: formattedUpdatedAt });
+    try {
+        const { id } = req.params;
+        console.log(id)
+        const spot = await Spot.findById(id);
+        console.log(spot)
+        if (!spot) {
+            console.log('OOOPSPFAPPFPAPFDPAF')
+            next(new ErrorHandler(`That spot doesn't exist, you can add it though!`, 404));
+            res.redirect("spots/new");
+        }
+        const updatedAt = spot.updated_at;
+        const formattedUpdatedAt = DateTime.fromJSDate(updatedAt).toFormat("LLL dd yyyy");
+        res.render("spots/show", { spot, updatedAt: formattedUpdatedAt });
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.use((err, req, res, next) => {
+  const { status = 500, message = "this is not good (-_-｡)" } = err;
+  res.status(status).send(message);
 });
 
 
