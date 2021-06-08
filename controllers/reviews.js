8ca5f00752a7c0b6f003298cd17da7f4c81785d8
hisamparker <hisamparker@gmail.com>
2021-06-08 21:59:57 +0200

    moved review callbacks out of routes and into controller file

---
+++ controllers/reviews.js
@@ -0,0 +1,48 @@
+const Spot = require(`../models/spot.model`);
+const Review = require(`../models/review.model`);
+
+module.exports.createNewReview = async (req, res, next) => {
+    // console.log(`body: ${JSON.stringify(req.body)}, params : ${JSON.stringify(req.params)}`);
+    const reviewedSpot = await Spot.findById(req.params.id);
+    const review = new Review(req.body.review);
+    review.author = req.user._id;
+    reviewedSpot.reviews.push(review);
+    await review.save();
+    await reviewedSpot.save();
+    req.flash(`success`, `Thanks for reviewing ${reviewedSpot.name}`);
+    return res.redirect(`/spots/${reviewedSpot._id}`);
+};
+
+module.exports.renderEditReviewForm = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review) {
+        req.flash(`error`, `Sorry, review not found.`);
+        res.redirect(`/spots/${id}`);
+    }
+    res.render(`reviews/edit`, { review, spotId: id });
+};
+
+module.exports.editReview = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const updatedReview = await Review.findByIdAndUpdate(
+        reviewId,
+        { ...req.body.review },
+        // make sure to validate with schema on update
+        { runValidators: true }
+    );
+    await updatedReview.save();
+    req.flash(`success`, `You've successfully updated your review!`);
+    return res.redirect(`/spots/${id}`);
+};
+
+module.exports.destroyReview = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    // The $pull operator removes from an existing array all instances of a value or values that match a specified condition.
+    const spotWithDeletedReview = await Spot.findByIdAndUpdate(id, {
+        $pull: { reviews: reviewId },
+    });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash(`success`, `Your review of ${spotWithDeletedReview.name} was deleted.`);
+    return res.redirect(`/spots/${id}`);
+};
--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,80 +1,23 @@
 const { Router } = require(`express`);
-
 // Routes all get separate params, but I want to get spot._id from the spot route params, so I set mergeParams to true
 const router = new Router({ mergeParams: true });
-const { reviewSchema } = require(`../validationSchemas`);
 
-const Spot = require(`../models/spot.model`);
+const { reviewSchema } = require(`../validationSchemas`);
 const Review = require(`../models/review.model`);
+const reviews = require(`../controllers/reviews`);
+const Spot = require(`../models/spot.model`);
 const tryCatchWrapper = require(`../utils/tryCatchWrapper`);
 const { isLoggedIn } = require(`../middleware/isLoggedIn`);
 const { validateReview } = require(`../middleware/validateReview`);
 const { isReviewAuthor } = require(`../middleware/isReviewAuthor`);
 
-router.post(
-    `/`,
-    isLoggedIn,
-    validateReview,
-    tryCatchWrapper(async (req, res, next) => {
-        // console.log(`body: ${JSON.stringify(req.body)}, params : ${JSON.stringify(req.params)}`);
-        const reviewedSpot = await Spot.findById(req.params.id);
-        const review = new Review(req.body.review);
-        review.author = req.user._id;
-        reviewedSpot.reviews.push(review);
-        await review.save();
-        await reviewedSpot.save();
-        req.flash(`success`, `Thanks for reviewing ${reviewedSpot.name}`);
-        return res.redirect(`/spots/${reviewedSpot._id}`);
-    })
-);
+router.post(`/`, isLoggedIn, validateReview, tryCatchWrapper(reviews.createNewReview));
 
 // review route has both review id and spot id so info for the spot that the review is on is available... can pre-populate form with info from the review id, author will not change because only author can edit
-router.get(
-    `/:reviewId/edit`,
-    isLoggedIn,
-    isReviewAuthor,
-    tryCatchWrapper(async (req, res, next) => {
-        const { id, reviewId } = req.params;
-        const review = await Review.findById(reviewId);
-        if (!review) {
-            req.flash(`error`, `Sorry, review not found.`);
-            res.redirect(`/spots/${id}`);
-        }
-        res.render(`reviews/edit`, { review, spotId: id });
-    })
-);
+router.get(`/:reviewId/edit`, isLoggedIn, isReviewAuthor, tryCatchWrapper(reviews.renderEditReviewForm));
 
-router.put(
-    `/:reviewId/edit`,
-    isLoggedIn,
-    isReviewAuthor,
-    validateReview,
-    tryCatchWrapper(async (req, res, next) => {
-        const { id, reviewId } = req.params;
-        const updatedReview = await Review.findByIdAndUpdate(
-            reviewId,
-            { ...req.body.review },
-            // make sure to validate with schema on update
-            { runValidators: true }
-        );
-        await updatedReview.save();
-        req.flash(`success`, `You've successfully updated your review!`);
-        return res.redirect(`/spots/${id}`);
-    })
-);
+router.put(`/:reviewId/edit`, isLoggedIn, isReviewAuthor, validateReview, tryCatchWrapper(reviews.editReview));
 
-router.delete(
-    `/:reviewId`,
-    isLoggedIn,
-    isReviewAuthor,
-    tryCatchWrapper(async (req, res, next) => {
-        const { id, reviewId } = req.params;
-        // The $pull operator removes from an existing array all instances of a value or values that match a specified condition.
-        const spotWithDeletedReview = await Spot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-        await Review.findByIdAndDelete(reviewId);
-        req.flash(`success`, `Your review of ${spotWithDeletedReview.name} was deleted.`);
-        return res.redirect(`/spots/${id}`);
-    })
-);
+router.delete(`/:reviewId`, isLoggedIn, isReviewAuthor, tryCatchWrapper(reviews.destroyReview));
 
 module.exports = router;
