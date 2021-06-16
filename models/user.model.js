29d9e24ae4ab9c9076a048d56fe87c03e5fcc462
hisamparker <hisamparker@gmail.com>
2021-06-16 11:40:23 +0200

    email modifications in user model

--- models/user.model.js
+++ models/user.model.js
@@ -11,6 +11,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
+            match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         },
         authoredSpots: [
             {
